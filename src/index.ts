233ff356306f5d1ae74d452b77b90ece84ee3d82
Dmitry Egorov <dmitry.egorov@ottonova.de>
2020-02-19 16:49:25 +0100

    changes config.

--- src/index.ts
+++ src/index.ts
@@ -125,7 +125,7 @@ class App {
   }
 
   private listen(): void {
-    const port = process.env.APP_PORT || 3000;
+    const port = process.env.PORT || 3000;
     this.server.listen(port, () => {
       this.logger.info(`Server listening on port ${port}`);
       this.crawler.start();
