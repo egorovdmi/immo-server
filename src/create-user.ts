0036004250a1d76650062f1848f1416bccb73cb1
Dmitry Egorov <dmitry.egorov@ottonova.de>
2020-02-28 15:54:18 +0100

    changed create-user script to store user profiles data in the firebase.

--- src/create-user.ts
+++ src/create-user.ts
@@ -1,12 +1,40 @@
 import * as bcrypt from "bcrypt";
-import * as lowdb from "lowdb";
-import * as FileSync from "lowdb/adapters/FileSync";
 import * as prompts from "prompts";
 import { v4 as uuid } from "uuid";
+import * as dotenv from "dotenv";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+dotenv.config();
 
 const run = async () => {
-  const adapter = new FileSync("db.json");
-  const db = lowdb(adapter);
+  firebase.initializeApp({
+    messagingSenderId: process.env.APP_FIREBASE_SERVER_ID,
+    projectId: process.env.APP_FIREBASE_PROJECT_ID,
+    apiKey: process.env.APP_FIREBASE_API_KEY,
+    appId: process.env.APP_FIREBASE_APP_ID,
+    authDomain: process.env.APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.APP_FIREBASE_DATABASE_URL,
+    storageBucket: process.env.APP_FIREBASE_STORAGE_BUCKET
+  });
+
+  try {
+    await firebase
+      .auth()
+      .signInWithEmailAndPassword(
+        process.env.APP_FIREBASE_LOGIN,
+        process.env.APP_FIREBASE_PASSWORD
+      );
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+
+    // tslint:disable-next-line:no-console
+    console.log(
+      `Error has occured. Error code: ${errorCode}, message: ${errorMessage}.`
+    );
+  }
 
   const response = await prompts([
     {
@@ -30,19 +58,25 @@ const run = async () => {
   const id = uuid();
   const hash = await bcrypt.hash(response.password, 10);
 
-  if (
-    !db
-      .get("users")
-      .find({ email: response.email })
-      .value()
-  ) {
-    db.get("users")
-      .push({ email: response.email, hash, id, name: response.name })
-      .write();
+  const database = firebase.database();
+  const query = await database
+    .ref("/users")
+    .orderByChild("email")
+    .equalTo(response.email)
+    .once("value");
 
+  if (!query.val()) {
+    const databaseResponse = await database
+      .ref(`/users/${id}`)
+      .set({ email: response.email, hash, id, name: response.name });
     // tslint:disable-next-line:no-console
     console.log("User created");
+  } else {
+    // tslint:disable-next-line:no-console
+    console.log("User already exists.");
   }
+
+  process.exit();
 };
 
 run();
